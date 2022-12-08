1511f40b0716dc02c4ccb2403563e715496e8786
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2022-12-08 01:11:27 +0100

    Add files via upload

--- 1_First_Java-Database_project/src/First_Java-Database_project/First_Java-Database_project_Java-SQL/src/Main.java
+++ 1_First_Java-Database_project/src/First_Java-Database_project/First_Java-Database_project_Java-SQL/src/Main.java
@@ -7,15 +7,35 @@ public class Main
 {
     public static void main(String[] args) throws Exception
     {
+        // odniesienie do lokalnej bazy danych
         Class.forName("com.mysql.jdbc.Driver");
         Connection con = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/1_car-rental", "root", "");
+
         // odczyt danych z tabeli pracwnikow
         Statement stmt = con.createStatement();
 
+        // wykonanie kodu SQL: SELECT * FROM pracownik
         ResultSet rs = stmt.executeQuery("SELECT * FROM pracownik");
-
         while(rs.next())
-            System.out.println(rs.getString(3));
+            System.out.println(rs.getString(3)); // wyświetlenie nazwisk
+
+        System.out.println("");
+
+        // wykonanie kodu SQL: SELECT * FROM miejsce"
+        ResultSet lc = stmt.executeQuery("SELECT * FROM miejsce");
+        while(lc.next())
+            System.out.println(lc.getString(4)); // wświetlenie miast
+
+        // dodawanie nowego rekordu do bazy danych SQL
+        PreparedStatement stmtt = con.prepareStatement("INSERT INTO miejsce (ulica, numer, miasto, kod, telefon) VALUES (?, ?, ?, ?, ?)");
+
+        stmtt.setString(1, "Grunwaldza");
+        stmtt.setString(2, "170");
+        stmtt.setString(3, "Gdańsk");
+        stmtt.setString(4, "81-532");
+        stmtt.setString(5, "567-876-543");
+
+        stmtt.executeUpdate();
     }
 }
