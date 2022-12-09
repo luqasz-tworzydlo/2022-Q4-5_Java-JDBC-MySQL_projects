846668214828e6d244476150b418be28a0610622
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2022-12-10 00:37:02 +0100

    Add files via upload

--- 2_Music-Collection_project/src/Music-Collection_project/Music-Collection_project_Java-SQL/src/Main.java
+++ 2_Music-Collection_project/src/Music-Collection_project/Music-Collection_project_Java-SQL/src/Main.java
@@ -16,8 +16,46 @@ public class Main
         Connection con = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/4.1_music-collection", "root", "");
 
+        // dodanie nowego rekordu do tabeli [1.1]
+        PreparedStatement stmt1a = con.prepareStatement
+                ("INSERT INTO band (name, genre, albums) VALUES (?, ?, ?)");
+        ((PreparedStatement)stmt1a).setString(1, "LQ"); // nazwa wykonawcy
+        stmt1a.setString(2, "classic"); // rodzaj muzyki
+        stmt1a.setString(3, "CM-22"); // nazwa albumu/ów
+        stmt1a.executeUpdate();
+        // dodanie nowego rekordu do tabeli [1.2]
+        PreparedStatement stmt1b = con.prepareStatement
+                ("INSERT INTO band (name, genre, albums) VALUES (?, ?, ?)");
+        ((PreparedStatement)stmt1b).setString(1, "JTT"); // nazwa wykonawcy
+        stmt1b.setString(2, "modern"); // rodzaj muzyki
+        stmt1b.setString(3, "MM-22"); // nazwa albumu/ów
+        stmt1b.executeUpdate();
+
+        // dodanie nowego rekordu do tabeli [2.1]
+        PreparedStatement stmt2a = con.prepareStatement
+                ("INSERT INTO album (title, tracks, year) VALUES (?, ?, ?)");
+        ((PreparedStatement)stmt2a).setString(1, "LAlbum"); // nazwa albumu
+        stmt2a.setString(2, "21"); // ilość utworów
+        stmt2a.setString(3, "2012"); // rok albumu
+        stmt2a.executeUpdate();
+
+        // dodanie nowego rekordu do tabeli [2.2]
+        PreparedStatement stmt2b = con.prepareStatement
+                ("INSERT INTO album (title, tracks, year) VALUES (?, ?, ?)");
+        ((PreparedStatement)stmt2b).setString(1, "JAlbum"); // nazwa albumu
+        stmt2b.setString(2, "300"); // ilość utworów
+        stmt2b.setString(3, "2021"); // rok albumu
+        stmt2b.executeUpdate();
+
+        // dodanie nowego rekordu do tabeli [2.3]
+        PreparedStatement stmt2c = con.prepareStatement
+                ("INSERT INTO album (title, tracks, year) VALUES (?, ?, ?)");
+        ((PreparedStatement)stmt2c).setString(1, "LJ-Album"); // nazwa albumu
+        stmt2c.setString(2, "to be revealed"); // ilość utworów
+        stmt2c.setString(3, "2023"); // rok albumu
+        stmt2c.executeUpdate();
+
         var option_number = 0;
-        while (option_number != 6) {
             System.out.println("1. Dodaj wykonawcę");
             System.out.println("2. Dodaj album");
             System.out.println("3. Wypisz wykonawców");
@@ -44,33 +82,32 @@ public class Main
                 case 1:
                     addCreator();
                     break;
-            }
         }
     }
-    public static  void showSelectedAlbums()
+    public static  void showSelectedAlbums() // c1
     {
         System.out.println("Podaj rok, z którego mają być wypisane albumy:");
         var selectYear = 0;
         selectYear = sc.nextInt();
         // ???
     }
-    public static void showAlbums()
+    public static void showAlbums() // c2
     {
         System.out.println("Wszystkie albumy [z kządego roku]:");
         // ???
     }
 
-    public static void showCreators()
+    public static void showCreators() // c3
     {
         System.out.println("Lista wszystkich wykonawców:");
         // ???
     }
-    public static void addAlbum()
+    public static void addAlbum() // c4
     {
         System.out.println("Dodaj nowy album do bazy danych:");
         // ???
     }
-    public static void addCreator()
+    public static void addCreator() // c5
     {
         System.out.println("Ddaj nowego wykonawcę do bazy danych:");
         // ???
